7dd3cc6e113c9e73accab3c2258eb9fb99dca6cd
lagout <lagout@users.noreply.github.com>
2016-08-24 11:25:13 +0200

    update : php , mariadb

--- start-install.sh
+++ start-install.sh
@@ -157,9 +157,9 @@ then
 fi
 echo -e "$ROUGE""#########################################################"
 echo -e "$ROUGE""# This script install:                                  #"
-echo -e "$ROUGE""#            openssl : 1.0.1t                           #"
-echo -e "$ROUGE""#                php : 5.6.22                           #"
-echo -e "$ROUGE""#            MariaDB : 5.5.50                           #"
+echo -e "$ROUGE""#            openssl : 1.0.2h                           #"
+echo -e "$ROUGE""#                php : 5.6.25                           #"
+echo -e "$ROUGE""#            MariaDB : 5.5.51                           #"
 echo -e "$ROUGE""#              nginx : 1.10.1                           #"
 echo -e "$ROUGE""#   Install logs can be found in ./full-install/log     #"
 echo -e "$ROUGE""#########################################################"
